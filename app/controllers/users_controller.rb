58d6d0e76b88a24b92653430f59a624d841f2a2e
Wardayaz <wayaz107@gmail.com>
2020-10-12 04:03:42 -0500

    created users_controller and login/signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,29 @@
+require 'pry'
 class UsersController < ApplicationController
 
+get '/signup' do
+   erb :"/users/signup"
 end 
+
+post 'signup' do
+end 
+
+get '/login' do
+  erb :'/users/login'
+end 
+
+post '/login' do
+  @user = User.find_by(username: params[:username])
+   if @user && @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+  redirect ??
+   else
+    redirect '/login'
+   end 
+end 
+
+get '/logout' do
+  session.clear
+  redirect '/'
+ end 
+end
